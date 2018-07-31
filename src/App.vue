95fde31bf5f9106c2080c76fc9f30500ee9c3b20
Adam Killander <Adamkillander@hotmail.com>
2018-07-31 21:00:16 +0200

    Actually working
    Now it is actually working, next step will be Service workers, browserconfig.xml and manifest.json.

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,82 @@
 <template>
-  <b-container fluid id="app" class="bg-gradient-primary p-4 h-100">
-    <b-container class="bg-white p-4">
-    <h1>{{ h1 }}</h1>
-    <span>{{ desc }}</span>
-    <hr>
-    <b-row>
-      <b-col md="8">
-        <b-row class="my-1">
-          <b-col v-for="(input, index) in inputs" :key="input.title" md="6">
-            <div class="p-2 mb-2 shadow-sm">
-              <label class="lead" :for="`meta${index}`" v-b-tooltip.hover :title="input.description">{{ input.title }}:</label>
-              <b-input-group v-if="!input.options">
-                <b-form-input :id="`meta${index}`" :type="input.type" :placeholder="input.placeholder" v-model="input.value" :value="input.value"></b-form-input>
-              </b-input-group>
-              <b-form-select v-if="input.options" v-model="input.value" :options="input.options"/>
-              <i v-if="input.title == 'Orientation'" class="fas fa-mobile-alt"></i>
-            </div>
-          </b-col>
-        </b-row>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col md="12">
-        <h2>Code</h2>
-        <b-tabs fill tabs>
-          <b-tab title="Raw code" active>
-            <div id="Raw" class="p-2 bg-light border border-top-0 rounded-bottom">
-              <code class="d-block mb-2 text-success">// Copy and paste this into your HTML header"</code>
-              <code class="d-block mb-2" v-for="inputs in inputs" :key="inputs.key">
-                {{inputs.meta}}
-                {{inputs.value}}
-              </code>
-              <code class="d-block mb-2" v-text="`<meta name='${inputs.name}' content='${inputs.name}'>`">
-
-                  <!-- Generic -->
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-                  <!-- Android -->
-                  <meta name="theme-color" content="#333333" />
-                  <meta name="mobile-web-app-capable" content="yes">
-                  <link href="icon-192x192.png" rel="icon" sizes="192x192">
-                  <link href="icon-128x128.png" rel="icon" sizes="128x128">
-                  <!-- Windows   -->
-                  <meta name="msapplication-navbutton-color" content="#333333">
-                  <meta name="msapplication-TileColor" content="#333333">
-                  <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
-                  <meta name="msapplication-config" content="browserconfig.xml">
-                  <meta name="msapplication-tap-highlight" content="no">
-                  <!-- Pinned Sites  -->
-                  <meta name="application-name" content="Application Name">
-                  <meta name="msapplication-tooltip" content="Tooltip Text">
-                  <meta name="msapplication-starturl" content="/">
-                  <link href="path/to/icon.svg" rel="mask-icon" size="any" color="red">
-                  <!-- UC Mobile Browser  -->
-                  <meta name="full-screen" content="yes">
-                  <meta name="browsermode" content="application">
-                  <link href="images/icon-52x52.png" rel="apple-touch-icon-precomposed" sizes="57x57">
-                  <link href="images/icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
-                  <!-- iOS  -->
-                  <meta name="apple-mobile-web-app-title" content="AppTitle">
-                  <meta name="apple-mobile-web-app-capable" content="yes">
-                  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-                  <link href="touch-icon-iphone.png" rel="apple-touch-icon">
-                  <link href="touch-icon-ipad.png" rel="apple-touch-icon" sizes="76x76">
-                  <link href="touch-icon-iphone-retina.png" rel="apple-touch-icon" sizes="120x120">
-                  <link href="touch-icon-ipad-retina.png" rel="apple-touch-icon" sizes="152x152">
-              </code>
-            </div>
-          </b-tab>
-          <!--<b-tab title="Wordpress Code">
-            <div id="Wp" class="p-2 bg-light border border-top-0 rounded-bottom">
-              <code class="d-block mb-2 text-success">// Add this to your functions.php file"</code>
-              <code class="d-block mb-2 text-primary" v-text="wp.firstline"></code>
-              <code class="d-block mb-2" v-for="name in codeMeta" :key="name" v-text="`<meta name='${codeMeta.name}' content='${codeMeta.content}'>`"></code>
-              <code class="d-block mb-2 text-primary" v-text="wp.secondline"></code>
-            </div>
-          </b-tab>-->
-        </b-tabs>
-      </b-col>
-    </b-row>
+  <b-container fluid id="app" class="h-100">
+    <b-modal id="modal1" title="Bootstrap-Vue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <b-container fluid class="p-0">
+      <b-row class="bg-primary p-4 shadow">
+        <b-col sm="9" class="text-light">
+          <h1 class="display-4">{{ h1 }}</h1>
+          <span class="lead">{{ desc }}</span>
+        </b-col>
+        <b-col sm="3" class="text-center text-sm-right text-light mt-4 mt-sm-0">
+          <a class="text-light d-inline-block text-center" v-b-modal.modal1><i class="far mx-auto fa-question-circle fa-fw fa-4x d-block"></i><span class="d-block">Requirements</span></a>
+          <a class="text-light d-inline-block text-center" href="https://github.com/Adamkillander96/instant-app-maker" target="_blank"><i class="fab mx-auto fa-github fa-fw fa-4x d-block"></i><span class="d-block">GitHub</span></a>
+        </b-col>
+      </b-row>
+      <b-row class="mt-5 px-0 px-sm-5">
+        <b-col lg="6">
+          <b-row class="my-1">
+            <b-col v-for="(input, index) in inputs" :key="input.title" md="6">
+              <div class="p-2 mb-4 shadow-sm rounded">
+                <label class="lead" :for="`meta${index}`" v-b-tooltip.hover :title="input.description">{{ input.title }}:</label>
+                <b-input-group v-if="!input.options">
+                  <b-form-input class="border-0 shadow-sm bg-light" :id="`meta${index}`" :type="input.type" :placeholder="input.placeholder" v-model="input.value" :value="input.value"></b-form-input>
+                </b-input-group>
+                <b-form-select class="border-0 shadow-sm bg-light" v-if="input.options" v-model="input.value" :options="input.options"/>
+                <i v-if="input.title == 'Orientation'" class="fas fa-mobile-alt"></i>
+              </div>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col class="shadow" lg="6">
+          <h2 class="mt-4">Code</h2>
+          <hr>
+          <b-tabs fill pills>
+            <b-tab title="Raw code" active>
+              <div id="Raw" class="p-2 bg-white mt-2">
+                <code class="d-block mb-2 text-success">// Copy and paste this into your HTML header"</code>
+                <metacomponent v-bind:inputs="inputs" />
+              </div>
+            </b-tab>
+            <b-tab title="Wordpress Code">
+              <div id="Wp" class="p-2 bg-white mt-2">
+                <code class="d-block mb-2 text-success">// Add this to your functions.php file"</code>
+                <code class="d-block mb-2 text-primary" v-text="wp.firstline"></code>
+                <metacomponent v-bind:inputs="inputs" />
+                <code class="d-block mb-2 text-primary" v-text="wp.secondline"></code>
+              </div>
+            </b-tab>
+          </b-tabs>
+        </b-col>
+      </b-row>
     </b-container>
   </b-container>
 </template>
 
 <script>
+import metacomponent from './meta/tags.vue'
 export default {
   name: 'app',
+  components: { 
+    metacomponent,
+  },
   data () {
     return {
       h1: 'The Instant App Maker',
       desc: 'Turn your website into a downloadable app in no time!',
       inputs: [
-        { title: 'Title', meta: "application-name", meta: "apple-mobile-web-app-title", description: 'This is the full name of your app', type: 'text', value:'', placeholder: 'The Best App Ever' },
+        { title: 'Title', description: 'This is the full name of your app', type: 'text', value:'', placeholder: 'The Best App Ever' },
         { title: 'Short title', description: 'This is the short name of your app', type: 'text', value:'', placeholder: 'Best App' },
-        { title: 'Description', meta: "msapplication-tooltip", description: 'A short description of your app', type: 'text', value:'', placeholder: 'Simply the best on planet earth' },
-        { title: 'Theme-color', meta: "theme-color", meta: "msapplication-TileColor", meta:"msapplication-navbutton-color", description: 'Provide the apps color in HEX', type: 'text', value:'', placeholder: '#333333' },
+        { title: 'Description', type: 'text', value:'', placeholder: 'Simply the best on planet earth' },
+        { title: 'Theme-color', description: 'Provide the apps color in HEX', type: 'text', value:'', placeholder: '#333333' },
         { title: 'Apple status bar', description: 'Apple gives you 3 choices.', type: 'text', value:'Please select an option', placeholder: 'Select...', options: ['Please select an option', 'default', 'black', 'black-translucent']},
-        { title: 'Image', meta: "msapplication-TileImage", meta: "msapplication-starturl", description: 'The image must be in PNG format', type: 'url', placeholder: 'https://example.com/image.png' },
+        { title: 'Image', description: 'The image must be in PNG format', type: 'url', placeholder: 'https://example.com/image.png' },
+        { title: 'SVG', description: 'The image must be in SVG format', type: 'url', placeholder: 'https://example.com/image.svg' },
         { title: 'Orientation', description: 'Select what how the app will be presented', type: 'text', value:'Please select an option', placeholder: 'Select...', options: ['Please select an option', 'any', 'landscape', 'portrait']},
-        { title: 'Display', description: 'Decide what UI elememts to show', type: 'text', value:'Please select an option', placeholder: 'Select...', options: ['Please select an option', 'fullscreen', 'standalone', 'browser']}
+        { title: 'Display', description: 'Decide what UI elememts to show', type: 'text', value:'Please select an option', placeholder: 'Select...', options: ['Please select an option', 'fullscreen', 'standalone', 'browser']},
+        
       ],
-      appTitle  : null,
-      appName   : null,
-      appDesc   : null,
-      appColor  : null,
-      appStatBar: null,
-      appImg    : null,
-      appOrient : null,
-      appDisplay: null,
       wp: { 
         firstline: "function adams_app_metadata() { ?>",
         secondline: "<?php } add_action( 'wp_head', 'adams_app_metadata' );"
---
+++ src/meta/tags.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="meta-tags" class="p-2 bg-white mt-2">
+    <!-- Generic -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>`"></code></pre>
+    <!-- Android -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='theme-color' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='mobile-web-app-capable' content='yes'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='icon' sizes='128x128' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='icon' sizes='192x192' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <!-- Windows   -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-navbutton-color' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-TileColor' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-TileImage' content='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-config' content='browserconfig.xml'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-tap-highlight' content='no'>`"></code></pre>
+    <!-- Pinned Sites  -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='application-name' content='`"></code>{{inputs[0].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-tooltip' content='`"></code>{{inputs[2].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-starturl' content='/'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='mask-icon' size='any' href='`"></code>{{inputs[6].value}}<code v-text="`' color='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
+    <!-- UC Mobile Browser  -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='full-screen' content='yes'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='browsermode' content='application'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='72x72' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon-precomposed' sizes='57x57' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <!-- iOS  -->
+    <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-title' content='`"></code>{{inputs[0].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-capable' content='yes'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-status-bar-style' content='`"></code>{{inputs[4].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-startup-image' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='76x76' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='152x152' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='120x120' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+  </div>   
+</template>
+
+<script>
+export default {
+  props: ['inputs'],
+}
+</script>
