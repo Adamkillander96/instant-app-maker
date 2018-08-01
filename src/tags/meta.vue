bfce76019565a3ecba9f3cea4bb35083f1b2cf09
Adamkillander96 <34036252+Adamkillander96@users.noreply.github.com>
2018-08-01 15:18:28 +0200

    Meta/Link update

--- src/tags/meta.vue
+++ src/tags/meta.vue
@@ -5,8 +5,8 @@
     <code v-text="`<!-- Android -->`"></code>
     <pre class="d-block mb-2"><code v-text="`<meta name='theme-color' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='mobile-web-app-capable' content='yes'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='icon' sizes='128x128' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='icon' sizes='192x192' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='icon' sizes='128x128' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='icon' sizes='192x192' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
     <code v-text="`<!-- Windows -->`"></code>
     <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-navbutton-color' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-TileColor' content='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
@@ -17,23 +17,23 @@
     <pre class="d-block mb-2"><code v-text="`<meta name='application-name' content='`"></code>{{inputs[0].value}}<code v-text="`'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-tooltip' content='`"></code>{{inputs[2].value}}<code v-text="`'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='msapplication-starturl' content='/'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='mask-icon' size='any' href='`"></code>{{inputs[6].value}}<code v-text="`' color='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='mask-icon' size='any' href='`"></code>{{inputs[6].value}}<code v-text="`' color='`"></code>{{inputs[3].value}}<code v-text="`'>`"></code></pre>
     <code v-text="`<!-- UC Mobile Browser -->`"></code>
     <pre class="d-block mb-2"><code v-text="`<meta name='full-screen' content='yes'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='browsermode' content='application'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='72x72' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon-precomposed' sizes='57x57' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon' sizes='72x72' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon-precomposed' sizes='57x57' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
     <code v-text="`<!-- iOS -->`"></code>
     <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-title' content='`"></code>{{inputs[0].value}}<code v-text="`'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-capable' content='yes'>`"></code></pre>
     <pre class="d-block mb-2"><code v-text="`<meta name='apple-mobile-web-app-status-bar-style' content='`"></code>{{inputs[4].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-startup-image' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='76x76' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='152x152' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='apple-touch-icon' sizes='120x120' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-startup-image' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon' sizes='76x76' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon' sizes='152x152' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='apple-touch-icon' sizes='120x120' href='`"></code>{{inputs[5].value}}<code v-text="`'>`"></code></pre>
     <code v-text="`<!-- Manifest -->`"></code>
-    <pre class="d-block mb-2"><code v-text="`<meta rel='manifest' href='`"></code>{{inputs[9].value}}/manifest.json<code v-text="`'>`"></code></pre>
+    <pre class="d-block mb-2"><code v-text="`<link rel='manifest' href='`"></code>{{inputs[9].value}}/manifest.json<code v-text="`'>`"></code></pre>
   </div>   
 </template>
 
